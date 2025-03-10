7ec9e87be1080df32160f8121e161a3e0edf5363
Robert Kirkby <robertdkirkby@gmail.com>
2025-03-11 01:28:30 +1300

    add codes

---
+++ Codes/WorkshopOLGModel1_ReturnFn.m
@@ -0,0 +1,18 @@
+function F=WorkshopOLGModel1_ReturnFn(h,aprime,a,z,sigma,psi,eta,r,alpha,delta,kappa_j,agej, Jr)
+% first three entries are the action space
+
+F=-Inf;
+
+w=(1-alpha)*((r+delta)/alpha)^(alpha/(1-alpha));
+
+% budget constraint
+if agej<Jr % working
+    c=(1+r)*a +w*kappa_j*h*exp(z) - aprime;
+else % retired
+    c=(1+r)*a -aprime;
+end
+
+if c>0
+    % utility fn
+    F=(c^(1-sigma))/(1-sigma)-psi*(h^(1+eta))/(1+eta);
+end
