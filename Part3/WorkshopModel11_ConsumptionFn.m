953732faef6d2c7877ccadf865fdd55484edbac3
Robert Kirkby <robertdkirkby@gmail.com>
2026-01-21 14:09:49 +1300

    Delete Part3 directory

--- Part3/WorkshopModel11_ConsumptionFn.m
+++
@@ -1,9 +0,0 @@
-function c=WorkshopModel11_ConsumptionFn(d,aprime,a,clag,z,w,agej,Jr,pension,r,kappa_j)
-
-if agej<Jr % If working age
-    c=w*kappa_j*z+(1+r)*a-aprime; % Add z here
-else % Retirement
-    c=pension+(1+r)*a-aprime;
-end
-
-end
